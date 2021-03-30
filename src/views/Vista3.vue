34cf3410715f22fa9bb1c8754ccf8c4e5d544ea8
Henning Kiel <henning.kiel@capgemini.com>
2021-03-30 18:30:45 +0200

    Save favourites

--- src/views/Vista3.vue
+++ src/views/Vista3.vue
@@ -13,12 +13,61 @@
         class="button is-link"
       />
     </v-btn>
+
+    <v-dialog
+      v-model="showSaveListDialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="secondary"
+          v-bind="attrs"
+          v-on="on"
+        >
+          Guardar favoritos
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="headline grey lighten-2">
+          Guardar lista de favoritos
+        </v-card-title>
+
+
+        <v-card-text>
+          <v-text-field v-model="listName" label="Nombre de la lista"></v-text-field>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-btn
+            color="secondary"
+            text
+            @click="showSaveListDialog = false"
+          >
+            Cancelar
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="saveFavList"
+          >
+            Guardar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
   </div>
 </template>
 <script>
 import tableuser from "@/components/tableuser.vue";
 import { mapGetters } from "vuex";
 import moment from "moment";
+import favouriteLists from  "@/api/favouriteLists";
 
 export default {
   name: "Vista3",
@@ -43,6 +92,18 @@ export default {
     },
   },
 
-  methods: {},
+  data() {
+    return {
+      showSaveListDialog: false,
+      listName: "Mis favoritos"
+    }
+  },
+
+  methods: {
+    async saveFavList() {
+      await favouriteLists.saveList(this.listName, this.favourites);
+      this.showSaveListDialog = false;
+    }
+  },
 };
 </script>
